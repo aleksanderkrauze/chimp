b16ca345c804fa84d47510491f4a9bc5e80f7ee1
Aleksander Krauze <alkoszczak@gmail.com>
2022-05-23 18:52:56 +0200

    Using upper-case for chimp version macros

--- tests/version_macros_test/main.cpp
+++ tests/version_macros_test/main.cpp
@@ -3,8 +3,8 @@
 #include "chimp/chimp.h"
 
 int main() {
-  std::cout << chimp_VERSION_MAJOR << '.' << chimp_VERSION_MINOR << '.'
-            << chimp_VERSION_PATCH << '\n';
+  std::cout << CHIMP_VERSION_MAJOR << '.' << CHIMP_VERSION_MINOR << '.'
+            << CHIMP_VERSION_PATCH << '\n';
 
   return 0;
 }
