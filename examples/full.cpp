f1867be1995a4350afd6654f830b4ea8850754f6
Aleksander Krauze <alkoszczak@gmail.com>
2022-05-29 22:55:41 +0200

    Updated examples/full.cpp

--- examples/full.cpp
+++ examples/full.cpp
@@ -1,36 +1,39 @@
-#include <cassert>
+#include <iostream>
 
-#include "chimp/chimp.h"
+#include <chimp/chimp.h>
 
 int main(int argc, char* argv[]) {
-  std::shared_ptr<chimp::Arg> all_arg, num_arg;
+  try {
+    std::shared_ptr<chimp::Arg> all_arg, num_arg;
 
-  // clang-format off
-  auto app = chimp::App::builder("test")
-               .author("Aleksander Krauze")
-               .version("1.0.0")
-               .about("Simple app that doesn't do anything")
-               .arg(
-                  chimp::Arg::builder("all")
-                    .short_arg('a')
-                    .long_arg("all")
-                    .build(all_arg)
-                )
-               .arg(
-                  chimp::Arg::builder("number")
-                    .short_arg('n')
-                    .long_arg("number")
-                    .takes_value(true)
-                    .build(num_arg)
-               )
-               .build();
-  // clang-format on
+    // clang-format off
+    auto app = chimp::App::builder("test")
+                 .author("Aleksander Krauze")
+                 .version("1.0.0")
+                 .about("Simple app that doesn't do anything")
+                 .arg(
+                    chimp::Arg::builder("all")
+                      .short_arg('a')
+                      .long_arg("all")
+                      .build(all_arg)
+                  )
+                 .arg(
+                    chimp::Arg::builder("number")
+                      .short_arg('n')
+                      .long_arg("number")
+                      .takes_value(true)
+                      .build(num_arg)
+                 )
+                 .build();
+    // clang-format on
 
-  app.parse(argc, argv);
+    app.parse(argc, argv);
+  } catch (const chimp::Error& e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
 
-  // Use all_arg & num_arg
-  assert(!all_arg->is_positional());
-  assert(!num_arg->is_positional());
+  // Use parsed arguments
 
   return 0;
 }
